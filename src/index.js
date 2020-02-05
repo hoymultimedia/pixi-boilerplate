b5635324a48b0cbd9f0a4b2010ff8367b96a13f3
Jonas Törnqvist <jonas@hoy.se>
2020-02-05 20:01:06 +0100

    From scratch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import App from './app/App';
-
-window.onload = () => {
-  const app = document.getElementById('app');
-  setTimeout(() => {
-    App.instance.init(app);
-  }, 500);
-};
