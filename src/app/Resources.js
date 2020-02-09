1d834465b3676deb553eef6bb486771c9311b342
Jonas Törnqvist <jonas@hoy.se>
2020-02-09 18:10:48 +0100

    Clean up

--- src/app/Resources.js
+++ src/app/Resources.js
@@ -7,14 +7,9 @@ import robotoPng2x from 'assets/bmpfonts/roboto@2x.png';
 
 export default class Resources {
   constructor(application) {
-    const app = application;
     this.onLoaded = new Signal();
 
-    /**
-     * Loading bitmap fonts
-     * Adding both image sheet and forcing loader to load ony one file at the time. (concurrency = 1)
-     * This to prevent issues with webpack
-     */
+    const app = application;
     app.loader.concurrency = 1;
     if (window.devicePixelRatio === 2) {
       app.loader.add(robotoPng2x);
